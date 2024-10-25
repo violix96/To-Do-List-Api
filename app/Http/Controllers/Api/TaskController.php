cf7ce7a413be46938d1d06f340299e7d25ade893
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-10-25 21:51:14 +0200

    [UPD] created apicontroller end setted api routes

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'completed' => 'boolean',
+        ]);
+
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Task::destroy($id);
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('tasks', [TaskController::class, 'index']);
+Route::post('tasks', [TaskController::class, 'store']);
+Route::get('tasks/{id}', [TaskController::class, 'show']);
+Route::put('tasks/{id}', [TaskController::class, 'update']);
+Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
