85ba72542c09e88613b0629bb42a7bb94ea01ea0
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-10-27 19:44:42 +0100

    [UPD] simplify task routes with Route::resource

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('tasks', [TaskController::class, 'index']);
-Route::post('tasks', [TaskController::class, 'store']);
-Route::get('tasks/{id}', [TaskController::class, 'show']);
-Route::put('tasks/{id}', [TaskController::class, 'update']);
-Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
+Route::resource('tasks', TaskController::class);
